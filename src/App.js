2d93b9f325b5d1712f7e8f537052b66c9a705e33
Javier Ortiz <46825648+j2or@users.noreply.github.com>
2022-07-17 19:16:26 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+      <h2>Desde codesandbox. Prueba react</h2>
+    </div>
+  );
+}
